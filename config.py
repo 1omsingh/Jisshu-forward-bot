b6e4b2a75e7a6fee7aa8b95657f806b60a587a0b
1omsingh <114354808+1omsingh@users.noreply.github.com>
2024-08-04 14:29:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ 
 
 class Config:
-    API_ID = environ.get("API_ID", "21291")
-    API_HASH = environ.get("API_HASH", "ed3c9220b55911903f3fb61f3")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "6558508467Fy7bWSpD-2qd-LPH9tAnL2GW8T1Qe4") 
+    API_ID = environ.get("API_ID", "26426018")
+    API_HASH = environ.get("API_HASH", "db3addaeb6a7d9a1c8c4d5be80f68d8f")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7255526540:AAHr9ywX-k8QlG_fEv8n0wRLczwcKqnGYqI") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
-    DATABASE_URI = environ.get("DATABASE", "mongodb+rward@cluster0.pnwq7om.mongodb.net/")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6245042947').split()]
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://Om2323705:9yAxZzAOZm9kw1f6@cluster0.bduwozx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0mongodb+srv://Om2323705:oHu6KwfZ2Re7jGmf@cluster0.bduwozx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "om2323705")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6074862505').split()]
 
 class temp(object): 
     lock = {}
